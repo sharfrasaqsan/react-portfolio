f0489b99d9e7834bc2e6f520ec7b582d898b90c1
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2025-07-27 23:53:26 +0530

    about me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -26,16 +26,15 @@ const AboutMe = () => {
   const techSkills = [
     { icon: <FaHtml5 color="#E34F26" />, name: "HTML5" },
     { icon: <FaCss3Alt color="#1572B6" />, name: "CSS3" },
-    { icon: <SiTailwindcss color="#38B2AC" />, name: "Tailwind CSS" },
-    { icon: <SiBootstrap color="#7952B3" />, name: "Bootstrap" },
     { icon: <FaJs color="#F7DF1E" />, name: "JavaScript" },
-    { icon: <SiJquery color="#0769AD" />, name: "jQuery" },
     { icon: <FaReact color="#61DAFB" />, name: "React.js" },
-    { icon: <FaPhp color="#777BB4" />, name: "PHP" },
     { icon: <FaWordpress color="#21759B" />, name: "WordPress" },
     { icon: <SiMagento color="#EE672F" />, name: "Magento 2" },
-    { icon: <SiShopify color="#96BF48" />, name: "Shopify" },
     { icon: <SiMysql color="#00758F" />, name: "MySQL" },
+    { icon: <SiBootstrap color="#7952B3" />, name: "Bootstrap" },
+    { icon: <SiShopify color="#96BF48" />, name: "Shopify" },
+    { icon: <SiTailwindcss color="#38B2AC" />, name: "Tailwind CSS" },
+    { icon: <SiJquery color="#0769AD" />, name: "jQuery" },
     { icon: <SiPostgresql color="#336791" />, name: "PostgreSQL" },
     { icon: <SiMongodb color="#47A248" />, name: "MongoDB" },
     { icon: <FaGithub color="#181717" />, name: "GitHub" },
@@ -111,7 +110,7 @@ const AboutMe = () => {
           <br />
           University of Kelaniya, 2018 – 2022
           <br />
-          GPA: <strong>3.14</strong> (Second Class Upper)
+          GPA: <strong>3.14</strong> (Second Class)
         </p>
       </section>
 
@@ -147,18 +146,24 @@ const AboutMe = () => {
       <section className="mb-5">
         <h4 className="fw-semibold mb-3">Certifications</h4>
         <ul className="text-muted ps-3">
-          <li>Intro to Front-End Development – Coursera (Meta)</li>
-          <li>Web Design for Beginners – UOM</li>
-        </ul>
-      </section>
-
-      <section>
-        <h4 className="fw-semibold mb-3">Additional Information</h4>
-        <ul className="text-muted ps-3">
           <li>
-            Languages: English (Fluent), Tamil (Native), Sinhala (Beginner)
+            <a
+              href="https://www.coursera.org/account/accomplishments/certificate/RLQUSVH6U6VU"
+              className="text-decoration-none"
+            >
+              Intro to Front-End Development
+            </a>{" "}
+            – Coursera (Meta)
           </li>
-          <li>Availability: 6-Week Notice Period</li>
+          <li>
+            <a
+              href="https://open.uom.lk/mod/customcert/view.php?id=697&downloadown=1"
+              className="text-decoration-none"
+            >
+              Web Design for Beginners
+            </a>{" "}
+            – UOM
+          </li>
         </ul>
       </section>
     </section>
